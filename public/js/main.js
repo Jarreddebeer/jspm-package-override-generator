f1b911a228a87fda4c0274264251cc050294a1d8
Jarred de Beer <jarr2000@gmail.com>
2014-08-22 00:14:09 +0200

    Adding styling of input fields, rows, and static floating <pre> column with all the json text

--- public/js/main.js
+++ public/js/main.js
@@ -38,11 +38,11 @@ var $registry = $('input[name="registry"]')
 
 function getTemplate(name) {
     var templates = {
-        'map':          '<input type="text" />: <input type="text" /><button class="remove">Remove</button>',
-        'deps':         '<input type="text" /><button class="remove">Remove</button>',
-        'files':        '<input type="text" /><button class="remove">Remove</button>',
-        'ignore':       '<input type="text" /><button class="remove">Remove</button>',
-        'dependencies': '<input type="text" />: <input type="text" /><button class="remove">Remove</button>'
+        'map':          '<label>&nbsp;</label><input type="text" /><span>:</span><input type="text" /><button class="remove">Remove</button>',
+        'deps':         '<label>&nbsp;</label><input type="text" /><button class="remove">Remove</button>',
+        'files':        '<label>&nbsp;</label><input type="text" /><button class="remove">Remove</button>',
+        'ignore':       '<label>&nbsp;</label><input type="text" /><button class="remove">Remove</button>',
+        'dependencies': '<label>&nbsp;</label><input type="text" /><span>:</span><input type="text" /><button class="remove">Remove</button>'
     }
     return '<li>' + templates[name] + '</li>';
 }
@@ -106,9 +106,10 @@ var deps = [];
 function getArrayList($ul) {
     var ary = [];
     $ul.children().each(function(i, li) {
-        var v = $(li).children(0).val();
+        var v = $(li).children('input').val();
         if (v) ary.push(v);
     });
+    console.log(ary);
     return ary;
 }
 
@@ -119,8 +120,8 @@ function getArrayList($ul) {
 function getObjectList($ul) {
     obj = {};
     $ul.children().each(function(i, li) {
-        var n = $(li).children().eq(0).val()
-          , v = $(li).children().eq(1).val();
+        var n = $(li).children('input').eq(0).val()
+          , v = $(li).children('input').eq(1).val();
         if (n && v)
             obj[n] = v;
     });
@@ -299,9 +300,10 @@ function renderOverride() {
 
     if (!$.isEmptyObject(o)) {
         var out = JSON.stringify(o, null, 2);
-        $override_out.val(out);
+        out = syntaxHighlight(out)
+        $override_out.html(out);
     } else {
-        $override_out.val('');
+        $override_out.html('');
     }
 
 }
@@ -319,7 +321,8 @@ function renderRegistry() {
             break;
     }
     out += '\n...';
-    $registry_out.val(out);
+    out = syntaxHighlight(out);
+    $registry_out.html(out);
 }
 //
 // https://github.com/dir/repo -> dir
@@ -354,7 +357,8 @@ function renderDirectory() {
             out += repo;
     }
     out += '@' + ver + '.json';
-    $directory_out.val(out);
+    out = syntaxHighlight(out);
+    $directory_out.html(out);
 }
 
 function setRegistry() {
@@ -366,6 +370,38 @@ function setRegistry() {
 
 }
 
+// thanks Pumbaa80 http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+         json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
+function setResultColumnFixed() {
+    var $fixed = $('.fixed')
+      , $input = $('#input')
+      , left = $input.offset().left + $input.outerWidth()
+      , height = $(window).height();
+    $fixed.css('left', left);
+    $fixed.css('height', height);
+}
+setResultColumnFixed();
 
 var renderOverrideEvent  = new CustomEvent('render-override');
 var renderRegistryEvent  = new CustomEvent('render-registry');
@@ -382,316 +418,9 @@ $rows.on('change', 'input[type="radio"]', parseField);
 
 $rows.on('change', 'input[type="checkbox"].toggle', toggleSubfields);
 $rows.on('change', 'input[type="checkbox"]', function() { document.dispatchEvent(renderOverrideEvent) });
+$rows.on('change', 'select', function() { document.dispatchEvent(renderOverrideEvent) });
 $rows.on('click', 'button.add', addFieldRow);
 $rows.on('click', 'button.add', function() { document.dispatchEvent(renderOverrideEvent) });
 $rows.on('click', 'button.remove', remFieldRow);
 $rows.on('click', 'button.remove', function() { document.dispatchEvent(renderOverrideEvent) });
 
-
-/*
-
-
-
-
-    el: '#package',
-    events: {
-        'change input[name="github"]': 'setLocations',
-        'change input[name="version"]': 'setVersion'
-    },
-
-    setLocations: function(e) {
-        var url = e.target.value;
-        var a = document.createElement('a');
-        a.href = url;
-        var paths = a.pathname.split('/');
-        if (paths.length == 3) {
-            this.model.set('folder', paths[1]);
-            this.model.set('filename', paths[2]);
-        } else {
-            this.$('input[name="github"]').val('');
-            this.$('input[name="github"]').attr('placeholder', 'I think the URL was invalid.');
-        }
-    },
-    setVersion: function(e) { }
-
-var override_model = Backbone.Model.extend({
-    defaults: {
-        main: null,
-        browser: null,
-        registry: null,
-        dependencies: null,
-        format: null,
-        directories: null,
-        files: null,
-        ignore: null,
-        map: null,
-        shim: null,
-        buildConfig: null
-    }
-});
-
-var override_view = Backbone.View.extend({
-
-    el: '#override',
-
-    events: {
-        'keyup input[type="text"]': 'entryKeyup',
-        'change input[type="checkbox"]': 'toggleSubList',
-        'change input[type="text"]': 'plainTextChange',
-        //
-        'keyup input[name="main"]': 'setModelField',
-        'keyup input[name="browser"]': 'setModelField',
-        'change input[type="radio"]': 'setModelField',
-        'keyup input[name="dependencies"]': 'setModelField',
-        //
-        'change select[name="format"]': 'setModelField',
-        'keyup input[name="lib"]': 'parseDirectories',
-        'keyup input[name="dist"]': 'parseDirectories',
-        'keyup input[name="deps"]': 'parseShim',
-        'keyup input[name="exports"]': 'parseShim',
-        'keyup input[name="minify"]': 'parseBuildConfig',
-        'keyup input[name="transpile"]': 'parseBuildConfig',
-        //
-        'click button.add': 'addLi',
-        'click button.remove': 'removeLi',
-    },
-
-    initialize: function() {
-        this.model.on('change', this.generateOverride, this);
-    },
-
-    entryKeyup: function(e) {
-        this.activateEntry(e);
-        this.plainTextChange(e);
-        this.generateOverride();
-    },
-
-    activateEntry: function(e) {
-        // make sure not plain field
-        if (typeof e.target.attributes.name != 'unfefined') {
-            var v = e.target.value;
-            var active = v.length > 0;
-            $(e.target).prev().prop('checked', active);
-        }
-    },
-
-    setModelField: function(e) {
-        var field = e.target.attributes.name.value;
-        var value = e.target.value;
-        this.model.set(field, value)
-    },
-
-    plainTextChange: function(e) {
-        if (typeof e.target.attributes.name == 'undefined') {
-            field = $(e.target).parent().parent().parent()
-            .find('input[type="checkbox"]').attr('name').split('-')[0];
-
-            console.log(field);
-
-            switch (field) {
-                case 'files':
-                    this.parseFiles();
-                    break;
-                case 'ignore':
-                    this.parseIgnore();
-                    break;
-                case 'dependencies':
-                    this.parseDependencies();
-                    break;
-                case 'deps':
-                    this.parseShim();
-                    break;
-                case 'map':
-                    this.parseMap();
-                    break;
-            }
-        }
-    },
-
-    parseDirectories: function() {
-        var lib_set  = this.$('input[name="lib-toggle"]').is(':checked');
-        var dist_set = this.$('input[name="dist-toggle"]').is(':checked');
-        var d = {};
-        if (lib_set) {
-            d.lib  = $('input[name="lib"]').val();
-        }
-        if (dist_set) {
-            d.dist = $('input[name="dist"]').val();
-        }
-        if (!lib_set && !dist_set) {
-            d = null;
-        }
-        this.model.set('directories', d);
-    },
-
-    parseDependencies: function() {
-        console.log('parsing dependencies');
-        var hash = {};
-        this.$('#dependencies').children().each(function(i, li) {
-            var inputs = $(li).find('input');
-            if (inputs[0].value.length > 0 && inputs[1].value.length > 0) {
-                hash[inputs[0].value] = inputs[1].value;
-            }
-        });
-        this.model.set('dependencies', hash);
-    },
-
-    parseFiles: function() {
-        var ary = [];
-        this.$('#files').children().each(function(i, li) {
-            var val = $(li).children(0).val();
-            if (val.length > 0) {
-                ary.push(val);
-            }
-        });
-        if (ary.length > 0) {
-            this.model.set('files', ary);
-        } else {
-            this.model.set('files', null);
-        }
-    },
-
-    parseIgnore: function() {
-        var ary = [];
-        this.$('#ignore').children().each(function(i, li) {
-            var val = $(li).children(0).val();
-            if (val.length > 0) {
-                ary.push(val);
-            }
-        });
-        if (ary.length > 0) {
-            this.model.set('ignore', ary);
-        } else {
-            this.model.set('ignore', null);
-        }
-    },
-
-    parseMap: function() {
-        console.log('parsing map');
-        var hash = {};
-        this.$('#map').children().each(function(i, li) {
-            var inputs = $(li).find('input');
-            if (inputs[0].value.length > 0 && inputs[1].value.length > 0) {
-                hash[inputs[0].value] = inputs[1].value;
-            }
-        });
-        this.model.set('map', hash);
-    },
-
-    parseShim: function() {
-        var dep_set = this.$('input[name="deps-toggle"]').is(':checked');
-        var exp_set = this.$('input[name="exports-toggle"]').is(':checked');
-        var $dep_ul = this.$('#deps');
-        var deps = this.eachInputsVal($dep_ul);
-        var obj;
-        if (dep_set && !exp_set) {
-            obj = deps;
-        } else if (dep_set && exp_set) {
-            var exp = this.$('input[name="exports"]').val();
-            obj = {deps: deps, exports: exp}
-        }
-        this.model.set('shim', obj);
-    },
-
-    parseBuildConfig: function() {
-        var ugl_set = this.$('input[name="minify"]').is(':checked');
-        var tra_set = this.$('input[name="transpile"]').is(':checked');
-        if (!ugl_set && !tra_set) {
-            this.model.set('buildConfig', null);
-        } else {
-            var obj = {};
-            if (ugl_set) {
-                obj.minify = true;
-            }
-            if (tra_set) {
-                obj.transpile = true;
-            }
-            this.model.set('buildConfig', obj);
-        }
-    },
-
-    // returns an array of values from each input in a list
-    eachInputsVal: function($ul) {
-        var ary = [],
-            val;
-        $ul.children().each(function(i, li) {
-            val = $(li).children(0).val();
-            if (val.length > 0) {
-                ary.push(val);
-            }
-        });
-        return ary;
-    },
-
-    toggleSubList: function(e) {
-        $e = $(e.target);
-        $p = $e.parent()
-        $p.find('> ul').toggleClass('visible');
-        if (!$e.is(':checked')) {
-            this.clearEntryBlock($e);
-        }
-    },
-
-    clearEntryBlock: function($e) {
-        $ul = $e.parent().find('> ul');
-        $ul.find('input[type="text"]').val('');
-        $ul.find('input[type="checkbox"]').prop('checked', false);
-        var name = $e.attr('name').split('-')[0];
-        // clear added entry fields
-        switch (name) {
-            case 'files':
-                $ul.children().each(function(i, li) {
-                    if (i != 0) {
-                        $(li).remove();
-                    }
-                });
-        }
-        //
-        this.model.set(name, null);
-    },
-
-    addLi: function(e) {
-        e.preventDefault();
-        $ul = this.getTargetUl(e);
-        var i = $ul.children().length;
-        var tpl = $ul.children(0).html()
-        $ul.append('<li>' + tpl + '</li>');
-    },
-
-    removeLi: function(e) {
-        e.preventDefault();
-        $ul = this.getTargetUl(e);
-        if ($ul.children().length > 1) {
-            $ul.children().last().remove();
-        }
-    },
-
-    getTargetUl: function(e) {
-        return $(e.target).parent().find('ul');
-    },
-
-    generateOverride: function() {
-        json = this.model.toJSON();
-        out_obj = {};
-        _.each(Object.keys(json), function(key) {
-            if (typeof json[key] != undefined && json[key] != null && json[key] != '') {
-                out_obj[key] = json[key];
-            }
-        });
-        var out;
-        if (Object.keys(out_obj).length > 0) {
-            out = JSON.stringify(out_obj, null, 2);
-        } else {
-            out = '';
-        }
-        $('#output').val(out);
-    }
-
-});
-
-var pm = new package_model()
-  , pv = new package_view({model: pm});
-  , om = new override_model();
-  , ov = new override_view({model: om});
-
-*/
