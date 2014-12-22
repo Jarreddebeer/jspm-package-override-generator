7b6fed25d003165cdbe9154487c12eed682b978a
Jarred de Beer <jarr2000@gmail.com>
2014-12-22 05:05:47 +0200

    adding cache check

--- public/js/main.js
+++ public/js/main.js
@@ -395,3 +395,24 @@ function setRenderColumnFixed() {
     $input.css('height', height);
 }
 setRenderColumnFixed();
+
+// clear the cache if outdated in the browser (using Github pages).
+// Thanks to: http://stackoverflow.com/questions/12556593/determining-a-page-is-outdated-on-github-pages
+
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      // Swap it in and reload the page to get the new hotness.
+      window.applicationCache.swapCache();
+      if (confirm('A new version of this site is available. Load it?')) {
+        window.location.reload();
+      }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+
+}, false);
